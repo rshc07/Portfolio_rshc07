4e02b7dadf73d8f72a364f283aadf27c178b6e1d
HEMACHANDRAN RS <rshc07@gmail.com>
2024-12-28 20:16:22 +0530

    About me has been aligned

--- Script.js
+++ Script.js
@@ -44,3 +44,16 @@ function submitToGoogleSheets(event) {
 //         }
 //     });
 // });
+
+// Smooth scrolling for anchor links
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
